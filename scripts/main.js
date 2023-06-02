b9c95c041ef114483627b6a1eda69d00c1b2aead
AshCat31 <131414743+AshCat31@users.noreply.github.com>
2023-06-01 21:47:57 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/11.png") {
+    myImage.setAttribute("src", "images/12.jfif");
+  } else {
+    myImage.setAttribute("src", "images/11.png");
+  }
+};
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 function setUserName() {
